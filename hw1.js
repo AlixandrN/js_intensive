c4faa8b53d717e2325624621a01d1fa4322f2ee0
Alexandr <nasanovicha@gmail.com>
2022-04-20 11:27:20 +0300

    feat: add solution Task1

--- hw1.js
+++ hw1.js
@@ -1,1 +1,10 @@
-//JS HomeWork-1
+//JS HomeWork-1 Task #1
+
+let a = Number(prompt('Введите первое число'))
+let b = Number(prompt('Введите систему счисления для числа'))
+if (!Number.isInteger(a) || !Number.isInteger(b) || b < 2 || b > 36) {
+    alert('Некорректный ввод!')
+}
+else {
+    alert(`Ответ: число ${a} в ${b}-ой системе счисления = ${a.toString(b)}`)
+}
